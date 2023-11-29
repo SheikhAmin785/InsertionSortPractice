ad0e64bd953871638b111a631d75ecb0a431136d
SheikhAmin785 <shakil785ks@gmail.com>
2023-11-30 00:40:42 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InsertionSortPractice
+{
+    class Program
+    {
+        public static void Insertionn(int[] arr)
+        {
+            int n = arr.Length;
+            int temp;
+            for (int i = 0; i < n; i++)
+            {
+                temp = arr[i];
+                int j =i-1;
+                while (j >= 0 && arr[j] > temp)
+
+                  {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = temp;
+            }
+        }
+        static void Main()
+        {
+            int[] array = new int[] { 9, 1, 8, 2, 7, 3, 6, 4, 5 };
+            Program.Insertionn(array);
+            for (int i = 0; i < array.Length; i++)
+            Console.WriteLine(" " + array[i]);
+        }
+
+    }
+}
